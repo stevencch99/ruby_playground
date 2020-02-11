e5fd4b634315bef4db0782ff8f5f82d6993c0d63
Steven C <stevencch99@gmail.com>
2020-02-11 09:36:13 +0800

    initial commit

---
+++ 104.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'nokogiri'
+require 'pry'
+
+uri = URI('https://www.104.com.tw/jobs/search/?keyword=rails')
+request = Net::HTTP::Get.new(uri)
+response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+  http.request(request)
+end
+
+# arr = []
+# Nokogiri.HTML(response.body).xpath('//a').each { |node| arr << node if /company/ =~ node.attributes['href'] }
+# p arr
+# arr.count
+
+company_list = Nokogiri.HTML(response.body).xpath('//a').select{ |node| /company/ =~ node.attributes['href'] }
+
+res = company_list.each_with_index.reduce({}) do |hash, company|
+  hash.merge!({
+    company[1] => [
+    company[0].attributes["title"].value.split[0][4..-1],
+    company[0].attributes["title"].value.split[1][5..-1],
+    company[0].attributes["href"].value
+  ]})
+end
+# binding.pry
+p resd
+# page_select = doc.css("select[class~='page-select']")
