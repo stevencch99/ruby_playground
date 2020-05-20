22cae47d20c1096a310262380c8870682b9444fc
Steven Chang <stevenchang@osp.com.tw>
2020-05-20 14:27:32 +0200

    Add ip crawler for work.

---
+++ ip_crawler.rb
@@ -0,0 +1,18 @@
+require 'net/http'
+require 'nokogiri'
+# require 'pry'
+
+class Crawler
+  attr_reader :response, :result
+
+  def initialize
+    uri = URI('https://jlkenixiel.execute-api.ap-northeast-1.amazonaws.com/test/getallserverip')
+    request = Net::HTTP::Get.new(uri)
+    @response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      http.request(request)
+    end
+
+    ip_list = Nokogiri.HTML(@response.body)
+    result = ip_list.search('body').to_s.split('<br>')
+  end
+end
